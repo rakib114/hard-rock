2cd259e3794232123bab17146706fff7178fb17c
rakib114 <mahmud17209@gmail.com>
2021-03-28 02:01:25 +0600

    solution of Hard Rock Music web page

---
+++ app.js
@@ -0,0 +1,70 @@
+//1st way to calling API
+const searchSong = async () => {
+  const searchText = document.getElementById("search-filed").value.trim();
+  const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    dispalySongs(data.data);
+  } catch (error) {
+    displayError("Somthing Went Wrong..... Please Try again Letter");
+  }
+};
+
+const dispalySongs = (songs) => {
+  const songContainer = document.getElementById("song-container");
+  songContainer.style.display = "block";
+  songContainer.innerHTML = "";
+  songs.forEach((song) => {
+    const songDiv = document.createElement("div");
+    songDiv.className = "single-result row align-items-center my-3 p-3";
+    songDiv.innerHTML = `
+      <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                 <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+     </div>
+     <div class="col-md-3 text-md-right text-center">
+            <button onclick ="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+     </div>
+      `;
+    songContainer.appendChild(songDiv);
+  });
+  const songLyric = document.getElementById("song-lyrics");
+  songLyric.style.display = "none";
+};
+
+const getLyric = async (artist, title) => {
+  const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLyric(data.lyrics);
+  } catch (error) {
+    displayError("Sorry I Can't found the lyrics.. Pleas try again Letter");
+  }
+  //2nd way to calling API
+  /** 
+   fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLyric(data.lyrics))
+    .catch(
+      displayError("Sorry I Can't found the lyrics.. Pleas try again Letter")
+    );
+*/
+  const songContainer = document.getElementById("song-container");
+  songContainer.style.display = "none";
+};
+
+const displayLyric = (lyrics) => {
+  const lyricDiv = document.getElementById("song-lyrics");
+  lyricDiv.innerText = lyrics;
+  lyricDiv.style.display = "block";
+};
+
+const displayError = (error) => {
+  const errorTag = document.getElementById("error-msg");
+  errorTag.innerText = error;
+};
